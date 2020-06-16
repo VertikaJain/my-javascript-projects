5a7734896fc3ad3d6e0017f4756be83313d98b6f
Vertika Jain <vertika.jain@digi-corp.com>
2020-06-17 01:19:50 +0530

    first commit

---
+++ color-flipper/hex.js
@@ -0,0 +1,16 @@
+const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
+const btn=document.getElementById("btn");
+const color=document.querySelector(".color");
+
+btn.addEventListener('click',()=>{
+    let hexColor="#";
+    for(let i=0;i<6;i++){
+        hexColor+=hex[getRandomNumber()];
+    }
+    document.body.style.backgroundColor=hexColor;
+    color.textContent=hexColor;
+})
+
+getRandomNumber=()=>{
+    return Math.floor(Math.random()*hex.length);
+}
