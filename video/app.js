e6b67edf30a89671547b93bc6a7b405951c8fb3a
Vertika Jain <vertikajain96@gmail.com>
2020-06-23 13:12:35 +0530

    Video Content Update

--- video/app.js
+++ video/app.js
@@ -10,7 +10,6 @@ switchBtn.addEventListener("click", () => {
         videoContainer.play();
 
     }
-    console.log(switchBtn.classList);
 })
 
 // The DOMContentLoaded event fires when the initial HTML document has been completely loaded and parsed, without waiting for stylesheets, images, and subframes to finish loading.
