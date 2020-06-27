3ec03872a193a3e9031243897ad53419ba9c0f02
Vertika Jain <vertikajain96@gmail.com>
2020-06-28 03:14:47 +0530

    Added Clear Button & Delete Button functionalities

--- todo-list-crud/app.js
+++ todo-list-crud/app.js
@@ -12,17 +12,17 @@ let editElement;
 let editId = "";
 
 // ****** EVENT LISTENERS **********
-
-form.addEventListener("submit", (e) => {
-    e.preventDefault();
+// to add elements to the list : on form submission
+form.addEventListener("submit", (event) => {
+    event.preventDefault();
     const toDoValue = toDo.value;
-    const id = new Date().getTime().toString();
+    const id = new Date().getTime().toString(); // to get unique value for id, date() can be used.
     if (toDoValue) {
         if (!editFlag) { // to add item to the list dynamically
             let element = document.createElement("article"); // creating new article to add to list
             element.classList.add("grocery-item"); // add class
             const attribute = document.createAttribute("data-id"); // creating attribute 
-            attribute.value = id; // 
+            attribute.value = id;
             element.setAttributeNode(attribute); // set that attribute for the created element
             element.innerHTML = `<p class="title">${toDoValue}</p>
             <div class="btn-container">
@@ -34,9 +34,16 @@ form.addEventListener("submit", (e) => {
               </button>
             </div>`;
             list.appendChild(element); // add whole element to the toDo list
-            container.classList.add("show-container");
+            container.classList.add("show-container"); // since visibility is false
             setAlert("Item/Task added to the list.", "success");
-
+            // edit and delete button elements
+            const editBtn = element.querySelector(".edit-btn");
+            editBtn.addEventListener("click", editItem);
+            const deleteBtn = element.querySelector(".delete-btn");
+            deleteBtn.addEventListener("click", deleteItem);
+            // CRUD
+            addToLocalStorage(id, toDoValue);
+            setToDefault();
         }
         else { // to edit items in the list
             setAlert("editing", "success");
@@ -45,27 +52,58 @@ form.addEventListener("submit", (e) => {
     } else { // if there is no value to edit
         setAlert("Please Enter Item/Task.", "danger");
     }
+});
 
-
+// to delete all elements from list
+clearBtn.addEventListener("click", () => {
+    const groceryItems = document.querySelectorAll(".grocery-item");
+    if (groceryItems.length > 0) {
+        for (let groceryItem of groceryItems) {
+            list.removeChild(groceryItem);
+        }
+    }
+    container.classList.remove("show-container");
+    setAlert("Items have been removed from the list.", "danger");
+    setToDefault(); // for edit functionality
+    // localStorage.removeItem("list");
 });
 
 // ****** FUNCTIONS **********
-
 setAlert = (text, styleClass) => {
     alert.textContent = text;
     alert.classList.add(`alert-${styleClass}`);
-
     setTimeout(() => {
         alert.textContent = ``;
         alert.classList.remove(`alert-${styleClass}`);
     }, 1000);
-
-
+}
+setToDefault = () => {
+    toDo.value = "";
+    editFlag = false;
+    editId = "";
+    submitBtn.textContent = "submit";
+};
+editItem = (event) => {
+}
+deleteItem = (event) => {
+    const delElement = event.currentTarget.parentElement.parentElement;
+    const id = delElement.dataset.id;
+    list.removeChild(delElement);
+    if (list.children.length === 0) {
+        container.classList.remove("show-container");
+    }
+    setAlert("Item successfully removed from list.", "success");
+    setToDefault();
+    removeFromLocalStorage(id);
 }
 
-
 // ****** LOCAL STORAGE **********
-
+addToLocalStorage = (id, value) => {
+    // console.log("add to local storage", id);
+}
+removeFromLocalStorage = (id) => {
+    // console.log("removeFromLocalStorage", id);
+}
 // ****** SETUP ITEMS **********
 
 
@@ -75,4 +113,6 @@ setAlert = (text, styleClass) => {
     1. changing content and classlist dynamically
     2. setTimeOut() method
     3. createElement, createAttribute, setAttributeNode
+    4. appendChild(), removeChild()
+    5. children property of array
 */
