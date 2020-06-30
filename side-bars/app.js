d3343d9adf1bcb04d224b640c83e8e62d547b571
Vertika Jain <vertikajain96@gmail.com>
2020-06-30 19:30:26 +0530

    Added preventDefault() and Social-link Updates

--- side-bars/app.js
+++ side-bars/app.js
@@ -1,7 +1,10 @@
 const sidebarToggle = document.querySelector(".sidebar-toggle");
 const sideBar = document.querySelector(".sidebar");
 const closeBtn = document.querySelector(".close-btn");
-
+const links = document.querySelector(".links");
+links.addEventListener("click", (e) => {
+    e.preventDefault();
+})
 sidebarToggle.addEventListener("click", () => {
     sideBar.classList.toggle("show-sidebar");
 })
