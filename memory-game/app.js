8fac9da824e415693ddeb203319e8bcd726fe6a1
Vertika Jain <vertikajain96@gmail.com>
2020-07-01 23:55:59 +0530

    Memory Game Project

---
+++ memory-game/app.js
@@ -0,0 +1,66 @@
+document.addEventListener('DOMContentLoaded', () => {
+  //card options
+  const cardArray = [
+    {
+      name: 'fries',
+      img: 'images/fries.png'
+    },
+    {
+      name: 'cheeseburger',
+      img: 'images/cheeseburger.png'
+    },
+    {
+      name: 'ice-cream',
+      img: 'images/ice-cream.png'
+    },
+    {
+      name: 'pizza',
+      img: 'images/pizza.png'
+    },
+    {
+      name: 'milkshake',
+      img: 'images/milkshake.png'
+    },
+    {
+      name: 'hotdog',
+      img: 'images/hotdog.png'
+    },
+    {
+      name: 'fries',
+      img: 'images/fries.png'
+    },
+    {
+      name: 'cheeseburger',
+      img: 'images/cheeseburger.png'
+    },
+    {
+      name: 'ice-cream',
+      img: 'images/ice-cream.png'
+    },
+    {
+      name: 'pizza',
+      img: 'images/pizza.png'
+    },
+    {
+      name: 'milkshake',
+      img: 'images/milkshake.png'
+    },
+    {
+      name: 'hotdog',
+      img: 'images/hotdog.png'
+    }
+  ]
+
+  const grid = document.querySelector(".grid");
+
+  createBoard = () => {
+    for (let index in cardArray) {
+      let element = document.createElement("img");
+      element.setAttribute("src", "images/logo.png");
+      element.setAttribute("data-id", index);
+      grid.append(element);
+    }
+  }
+
+  createBoard();
+});
