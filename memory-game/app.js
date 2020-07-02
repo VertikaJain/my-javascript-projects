c9e98248a1e9030a97e0009e380ebcfbcf491028
Vertika Jain <vertikajain96@gmail.com>
2020-07-02 20:18:52 +0530

    Added flipCard & CheckForMatch functionalities

--- memory-game/app.js
+++ memory-game/app.js
@@ -1,66 +1,132 @@
-document.addEventListener('DOMContentLoaded', () => {
-  //card options
-  const cardArray = [
-    {
-      name: 'fries',
-      img: 'images/fries.png'
-    },
-    {
-      name: 'cheeseburger',
-      img: 'images/cheeseburger.png'
-    },
-    {
-      name: 'ice-cream',
-      img: 'images/ice-cream.png'
-    },
-    {
-      name: 'pizza',
-      img: 'images/pizza.png'
-    },
-    {
-      name: 'milkshake',
-      img: 'images/milkshake.png'
-    },
-    {
-      name: 'hotdog',
-      img: 'images/hotdog.png'
-    },
-    {
-      name: 'fries',
-      img: 'images/fries.png'
-    },
-    {
-      name: 'cheeseburger',
-      img: 'images/cheeseburger.png'
-    },
-    {
-      name: 'ice-cream',
-      img: 'images/ice-cream.png'
-    },
-    {
-      name: 'pizza',
-      img: 'images/pizza.png'
-    },
-    {
-      name: 'milkshake',
-      img: 'images/milkshake.png'
-    },
-    {
-      name: 'hotdog',
-      img: 'images/hotdog.png'
-    }
-  ]
+const cardArray = [
+  {
+    name: 'angular',
+    img: 'images/angular.png'
+  },
+  {
+    name: 'vue',
+    img: 'images/vue.png'
+  },
+  {
+    name: 'node',
+    img: 'images/node.png'
+  },
+  {
+    name: 'ember',
+    img: 'images/ember.png'
+  },
+  {
+    name: 'react',
+    img: 'images/react.png'
+  },
+  {
+    name: 'backbone',
+    img: 'images/backbone.png'
+  },
+  {
+    name: 'angular',
+    img: 'images/angular.png'
+  },
+  {
+    name: 'vue',
+    img: 'images/vue.png'
+  },
+  {
+    name: 'node',
+    img: 'images/node.png'
+  },
+  {
+    name: 'ember',
+    img: 'images/ember.png'
+  },
+  {
+    name: 'react',
+    img: 'images/react.png'
+  },
+  {
+    name: 'backbone',
+    img: 'images/backbone.png'
+  }
+]
 
-  const grid = document.querySelector(".grid");
+let selectedCards = [], selectedCardIds = [], cardsWon = [];
+const grid = document.querySelector(".grid");
+const result = document.getElementById("result");
 
-  createBoard = () => {
-    for (let index in cardArray) {
-      let element = document.createElement("img");
-      element.setAttribute("src", "images/logo.png");
-      element.setAttribute("data-id", index);
-      grid.append(element);
+window.addEventListener("DOMContentLoaded", () => {
+  createBoard();
+})
+
+// ----- METHODS -----
+
+// method to create cards board for game setup.
+createBoard = () => {
+  for (let index in cardArray) {
+    let element = document.createElement("img");
+    element.setAttribute("src", "images/logo.png");
+    element.setAttribute("data-id", index);
+    grid.appendChild(element);
+    element.addEventListener("click", flipCard);
+  }
+}
+
+// method to flip card on click event.
+flipCard = (event) => {
+  let img = event.target;
+  const imgId = event.currentTarget.dataset.id; // get id of the box clicked
+  selectedCards.push(cardArray[imgId].name);
+  selectedCardIds.push(imgId);
+  img.setAttribute("src", cardArray[imgId].img); // flip image
+  if (selectedCards.length === 2) {
+    checkForMatch();
+  }
+}
+
+// method to check if 2 cards match.
+checkForMatch = () => {
+  const imgs = grid.querySelectorAll("img");
+  let id0 = selectedCardIds[0];
+  let id1 = selectedCardIds[1];
+  // CASE 1: IF SAME CARD IS CLICKED TWICE
+  if (id0 === id1) {
+    setLogo(id0, id1);
+  }
+  // CASE 2: ON CORRECT MATCH OF CARDS
+  else if (selectedCards[0] === selectedCards[1]) { // name
+    cardsWon.push(selectedCards);
+    // direct to black image 
+    for (let img of imgs) {
+      id = img.dataset.id;
+      if (id === id0 || id === id1) {
+        setTimeout(() => {
+          img.setAttribute("src", "images/black.jpg"); // flip image back after 1 second
+        }, 1000);
+
+        // block clicking on these images
+        img.removeEventListener("click", flipCard);
+      }
     }
   }
+  // CASE 3: ON INCORRECT MATCH OF CARDS
+  else {
+    setLogo(id0, id1);
+  }
+  // empty selected card array
+  selectedCards = [];
+  selectedCardIds = [];
+  result.textContent = cardsWon.length;
 
-  createBoard();
-});
+  // on completion of game
+  if (cardsWon.length === cardArray.length / 2) {
+    // alert 
+  }
+}
+
+// method to set logo image back after 1 second
+setLogo = (id0, id1) => {
+  const imgs = grid.querySelectorAll("img");
+  setTimeout(() => {
+    imgs[id0].setAttribute("src", "images/logo.png");
+    imgs[id1].setAttribute("src", "images/logo.png");
+  }, 1000);
+}
