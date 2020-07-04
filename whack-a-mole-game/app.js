dcedefa31492c9f13baf7e101360d06015134a3f
Vertika Jain <vertikajain96@gmail.com>
2020-07-04 21:51:04 +0530

    Added Mole at random to Grid. #day18

--- whack-a-mole-game/app.js
+++ whack-a-mole-game/app.js
@@ -1,7 +1,20 @@
 const grid = document.querySelector(".grid");
-console.log(grid);
+const mole = document.querySelectorAll(".mole");
+const score = document.getElementById("score");
+const timeLeft = document.getElementById("time-left");
+
+let result = 0;
+let currentTime = timeLeft.textContent; //60
 
 window.addEventListener("DOMContentLoaded", () => {
+    createGrid();
+    randomSquare();
+});
+
+// ------ METHODS ------
+
+// method to create grid squares
+createGrid = () => {
     for (let id = 1; id <= 9; id++) {
         let element = document.createElement("div");
         element.classList.add("square");
@@ -10,4 +23,19 @@ window.addEventListener("DOMContentLoaded", () => {
         element.setAttributeNode(attribute);
         grid.appendChild(element);
     }
-});
+}
+
+// method to show mole image at random squares & modify score.
+randomSquare = () => {
+    const squares = document.querySelectorAll(".square");
+    for (let square of squares) {
+        square.classList.remove("mole"); // removing mole image if previously assigned
+    }
+    let square = squares[Math.floor(Math.random() * 9)]; // adding mole image at random position on the grid
+    square.classList.add("mole");
+    square.addEventListener("click", () => {
+        result++;
+        score.textContent = result;
+
+    })
+}
