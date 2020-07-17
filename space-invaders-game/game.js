d6cbc6b5a5e68988e96b74e44a8a53d5a3a2e0d5
Vertika Jain <vertikajain96@gmail.com>
2020-07-17 12:36:06 +0530

    Updated checkBoundaries() to move.js module #Day31

--- space-invaders-game/game.js
+++ space-invaders-game/game.js
@@ -3,18 +3,10 @@ import { update as updatePlayer } from "./move.js";
 
 const gameContainer = document.querySelector(".game");
 const GAME_STATE = getGameStateInputs();
-const PLAYER_WIDTH = 20;
 
 // method to set position of player on every update & at game start.
 let setPosition = (player) => {
     player.style.transform = `translate(${GAME_STATE.x}px, ${GAME_STATE.y}px)`;
-}
-
-// method to ensure that the player does not cross the boundaries of the game board.
-let checkBoundaries = (v, vmin, vmax) => {
-    if (v < vmin) return vmin;
-    else if (v > vmax) return vmax;
-    else return v;
 }
 
 // method to create & place the player at the center of the game board.
@@ -33,7 +25,6 @@ let update = (currentTime) => {
     updatePlayer(deltaTime);
     const player = document.querySelector(".player");
     setPosition(player);
-    GAME_STATE.x = checkBoundaries(GAME_STATE.x, PLAYER_WIDTH, GAME_STATE.gameWidth - PLAYER_WIDTH);
     GAME_STATE.lastTime = currentTime;
     window.requestAnimationFrame(update);
 }
--- space-invaders-game/move.js
+++ space-invaders-game/move.js
@@ -1,8 +1,17 @@
 import { getGameStateInputs } from "./input.js";
 const GAME_STATE = getGameStateInputs();
+const PLAYER_WIDTH = 20;
 
 // method calculates & returns the distance to be moved by the player, based on given speed & the time difference, i.e. delta time. (dis=t*s)
 export let update = (deltaTime) => {
     if (GAME_STATE.leftPress) GAME_STATE.x -= deltaTime * GAME_STATE.playerMaxSpeed;
     if (GAME_STATE.rightPress) GAME_STATE.x += deltaTime * GAME_STATE.playerMaxSpeed;
+    GAME_STATE.x = checkBoundaries(GAME_STATE.x, PLAYER_WIDTH, GAME_STATE.gameWidth - PLAYER_WIDTH);
+}
+
+// method to ensure that the player does not cross the boundaries of the game board.
+let checkBoundaries = (v, vmin, vmax) => {
+    if (v < vmin) return vmin;
+    else if (v > vmax) return vmax;
+    else return v;
 }
