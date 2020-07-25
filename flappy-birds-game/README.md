4a5845c04a9f239fcd129c49e75a16df1180d764
Vertika Jain <vertikajain96@gmail.com>
2020-07-25 11:26:07 +0530

    Update Pipes, Check Bird Collision & Add Score #Day37

--- flappy-birds-game/README.md
+++ flappy-birds-game/README.md
@@ -13,6 +13,14 @@
 2. Load image & audio content using new Image() & new Audio() respectively.
 3. Use src property to assign the media to variables.
 4. Create a draw() method which handles all the displaying of elements on the canvas, using the built-in drawImage() method.
+5. Use requestAnimationFrame(draw) to enable Game Loop.
+6. Create an array of pipes[], to draw multiple pipes on the canvas, by updating the x & y coordinates using Math.random().
+7. Use coordinates of the bird and pipes to check for collision of these elements.
+    1. If the bird's position on x-axis is equivant to the position of pipe or in-between the width of pipe, and
+    2. if the bird's position on y-axis is greater than or equal to the position of North pipe, OR if it is less than or equal to the position of South pipe
+    3. Check if the bird reaches the foreGround.
+8. Increase score once the pipe crosses the bird without collision.
+9. Display the updated score using fillText() on the canvas.
 
 
 ## Credits
--- flappy-birds-game/game.js
+++ flappy-birds-game/game.js
@@ -21,15 +21,49 @@ pipeSouth.src = "images/pipeSouth.png";
 fly.src = "sounds/fly.mp3";
 scr.src = "sounds/score.mp3";
 
-let gap = pipeNorth.height + 85; // gap b/w north & south pipes
-let gravity = 1;
+let birdState = { x: 10, y: 150 }; // coordinates of bird
+let pipes = [];
+pipes[0] = { x: canvasContainer.width, y: 0 };
+let score = 0;
 
+// move bird upwards on keydown event
+window.addEventListener("keydown", () => {
+    birdState.y -= 25;
+    fly.play();
+})
+
+// method to draw elements on the canvas
 let draw = () => {
     ctx.drawImage(backGround, 0, 0);
-    ctx.drawImage(pipeNorth, 100, 0);
-    ctx.drawImage(pipeSouth, 100, gap);
+    // move pipes along the game screen
+    for (let pipe of pipes) {
+        let gap = pipeNorth.height + 85; // gap b/w north & south pipes
+        ctx.drawImage(pipeNorth, pipe.x, pipe.y);
+        ctx.drawImage(pipeSouth, pipe.x, pipe.y + gap);
+        pipe.x--;
+        if (pipe.x === 125) {
+            pipes.push({
+                x: canvasContainer.width,
+                y: Math.floor(Math.random() * pipeNorth.height) - pipeNorth.height
+            });
+        }
+        // check collision of elements
+        if (birdState.x + bird.width >= pipe.x && birdState.x <= pipe.x + pipeNorth.width
+            && (birdState.y <= pipe.y + pipeNorth.height || birdState.y + bird.height >= pipe.y + gap)
+            || birdState.y + bird.height >= canvasContainer.height - foreGround.height) {
+            location.reload();
+        }
+        // increase score when pipe crosses the position of bird
+        if (pipe.x === 5) {
+            score++;
+            scr.play();
+        }
+    }
     ctx.drawImage(foreGround, 0, canvasContainer.height - foreGround.height); // to place foreGround image at the bottom of the canvas
-    ctx.drawImage(bird, 10, 150 + gravity++);
+    ctx.drawImage(bird, birdState.x, birdState.y += 1.5); // move the bird downward
+    ctx.fillStyle = "#000";
+    ctx.font = "20px Verdana";
+    ctx.fillText("Score : " + score, 10, canvasContainer.height - 20);
     requestAnimationFrame(draw);
 }
 
@@ -37,4 +71,7 @@ let draw = () => {
     Learnings:
     1. getContext() used to access the drawing context of the canvas element
     2. drawImage() draws an image, canvas, or video onto the canvas, at the specified positions.
+    3. fillStyle property is used to render the text in another color/gradient.
+    4. font property is used to specify font and font size.
+    5. fillText() draws filled text on the canvas.
 */
